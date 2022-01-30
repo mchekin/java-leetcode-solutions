ad75041547b64da130206d33e262d9838d764b1a
Michael Chekin <mchekin@gmail.com>
2022-01-30 12:09:20 +0100

    Longest Common Prefix ( Divide And Conquer - Time and Space Complexity )

--- src/com/mchekin/leetcodesolutions/longestcommonprefix/divideandconuer/Solution.java
+++ src/com/mchekin/leetcodesolutions/longestcommonprefix/divideandconuer/Solution.java
@@ -1,5 +1,12 @@
 package com.mchekin.leetcodesolutions.longestcommonprefix.divideandconuer;
 
+/**
+ * Problem: <a href="https://leetcode.com/problems/longest-common-prefix/">Longest Common Prefix</a>
+ *
+ * <p>
+ * Time Complexity: O(n*m) (where n is the number of strings and m is the number of characters a string)
+ * Space Complexity: O(m * log n)
+ */
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
 
