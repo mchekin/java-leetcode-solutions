fca15941f5221128b9fdf6cc9a8aa77ae7751f89
Michael Chekin <mchekin@gmail.com>
2022-02-06 15:03:33 +0100

    Best Time to Buy and Sell Stock

---
+++ src/com/mchekin/leetcodesolutions/besttimetobuyandsellstock/Solution.java
@@ -0,0 +1,31 @@
+package com.mchekin.leetcodesolutions.besttimetobuyandsellstock;
+
+/**
+ * Problem: <a href="https://leetcode.com/problems/best-time-to-buy-and-sell-stock/">Best Time to Buy and Sell Stock</a>
+ *
+ * <p>
+ * Time Complexity: O(n^2) (where n is the number of digits)
+ * Space Complexity: O(1)
+ */
+public class Solution {
+    public int maxProfit(int[] prices) {
+
+        if (prices == null) {
+            return 0;
+        }
+
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+
+                if (maxProfit < profit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+}
---
+++ test/com/mchekin/leetcodesolutions/besttimetobuyandsellstock/SolutionTest.java
@@ -0,0 +1,51 @@
+package com.mchekin.leetcodesolutions.besttimetobuyandsellstock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SolutionTest {
+    @ParameterizedTest
+    @MethodSource("provideExamples")
+    public void removesDuplicates(int[] input, int expected) {
+
+        assertEquals(expected, new Solution().maxProfit(input));
+    }
+
+    public static Stream<Arguments> provideExamples() {
+        return Stream.of(
+                Arguments.of(
+                        null,
+                        0
+                ),
+                Arguments.of(
+                        new int[]{},
+                        0
+                ),
+                Arguments.of(
+                        new int[]{7},
+                        0
+                ),
+                Arguments.of(
+                        new int[]{1, 7},
+                        6
+                ),
+                Arguments.of(
+                        new int[]{7, 1},
+                        0
+                ),
+                Arguments.of(
+                        new int[]{7,1,5,3,6,4},
+                        5
+                ),
+                Arguments.of(
+                        new int[]{7,6,4,3,1},
+                        0
+                )
+        );
+    }
+}
